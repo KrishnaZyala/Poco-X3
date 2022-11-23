245f3982bc28bc612053b897cc19c32c194a8e33
Krishna Singh <KrishnaZyala@gmail.com>
2022-11-23 21:40:23 +0530

    base

--- rm.sh
+++ rm.sh
@@ -1,69 +1,68 @@
-#!/bin/sh
-rm -r /cust/app
-rm -r /cust/cust
-rm -r /cust/etc
-rm -r /cust/lost+found
-rm -r /cust/prebuilts
+rm -v -r /cust/app
+rm -v -r /cust/cust
+rm -v -r /cust/etc
+rm -v -r /cust/lost+found
+rm -v -r /cust/prebuilts
 
 
-rm -r /product/app/Chrome
-rm -r /product/app/Gmail2
-rm -r /product/app/GoogleContacts
-rm -r /product/app/GoogleOne
-rm -r /product/app/GPayIndia
-rm -r /product/app/Maps
-rm -r /product/app/Messages
-rm -r /product/app/PhotoTable
-rm -r /product/app/YouTube
+rm -v -r /product/app/Chrome
+rm -v -r /product/app/Gmail2
+rm -v -r /product/app/GoogleContacts
+rm -v -r /product/app/GoogleOne
+rm -v -r /product/app/GPayIndia
+rm -v -r /product/app/Maps
+rm -v -r /product/app/Messages
+rm -v -r /product/app/PhotoTable
+rm -v -r /product/app/YouTube
 
 
-rm -r /product/priv-app/AndroidAutoStub
-rm -r /product/priv-app/GoogleAssistant
-rm -r /product/priv-app/Wellbeing
+rm -v -r /product/priv-app/AndroidAutoStub
+rm -v -r /product/priv-app/GoogleAssistant
+rm -v -r /product/priv-app/Wellbeing
 
 
-rm -r /system/app/AnalyticsCore
-rm -r /system/app/Calculator
-rm -r /system/app/CloudService
-rm -r /system/app/facebook-appmanager
-rm -r /system/app/FileExplorerGlobal
-rm -r /system/app/InMipay
-rm -r /system/app/Lens
-rm -r /system/app/mi_connect_service
-rm -r /system/app/MiCloudSync
-rm -r /system/app/MiLinkService2
-rm -r /system/app/MiPicks
-rm -r /system/app/MiPlayClient
-rm -r /system/app/MiuiBugReport
-rm -r /system/app/MiuiCompass
-rm -r /system/app/MiuiFrequentPhrase
-rm -r /system/app/Notes
-rm -r /system/app/PaymentService
-rm -r /system/app/PlayAutoInstallStubApp
-rm -r /system/app/SystemAppUpdater
-rm -r /system/app/XiaomiServiceFramework
+rm -v -r /system/app/AnalyticsCore
+rm -v -r /system/app/Calculator
+rm -v -r /system/app/CloudService
+rm -v -r /system/app/facebook-appmanager
+rm -v -r /system/app/FileExplorerGlobal
+rm -v -r /system/app/InMipay
+rm -v -r /system/app/Lens
+rm -v -r /system/app/mi_connect_service
+rm -v -r /system/app/MiCloudSync
+rm -v -r /system/app/MiLinkService2
+rm -v -r /system/app/MiPicks
+rm -v -r /system/app/MiPlayClient
+rm -v -r /system/app/MiuiBugReport
+rm -v -r /system/app/MiuiCompass
+rm -v -r /system/app/MiuiFrequentPhrase
+rm -v -r /system/app/Notes
+rm -v -r /system/app/PaymentService
+rm -v -r /system/app/PlayAutoInstallStubApp
+rm -v -r /system/app/SystemAppUpdater
+rm -v -r /system/app/XiaomiServiceFramework
 
 
-rm -r /system/data-app/MiCreditInStub
-rm -r /system/data-app/MiMediaEditor
+rm -v -r /system/data-app/MiCreditInStub
+rm -v -r /system/data-app/MiMediaEditor
 
-rm -r /system/priv-app/Calendar
-rm -r /system/priv-app/CleanMaster
-rm -r /system/priv-app/facebook-installer
-rm -r /system/priv-app/facebook-services
-rm -r /system/priv-app/MiCalendarGlobal
-rm -r /system/priv-app/MiDrop
-rm -r /system/priv-app/MiService
-rm -r /system/priv-app/MiShare
-rm -r /system/priv-app/MiuiExtraPhoto
-rm -r /system/priv-app/MiuiFreeformService
-rm -r /system/priv-app/MiuiMusicGlobal
-rm -r /system/priv-app/MiuiScanner
-rm -r /system/priv-app/Weather
-rm -r /system/priv-app/YellowPage
+rm -v -r /system/priv-app/Calendar
+rm -v -r /system/priv-app/CleanMaster
+rm -v -r /system/priv-app/facebook-installer
+rm -v -r /system/priv-app/facebook-services
+rm -v -r /system/priv-app/MiCalendarGlobal
+rm -v -r /system/priv-app/MiDrop
+rm -v -r /system/priv-app/MiService
+rm -v -r /system/priv-app/MiShare
+rm -v -r /system/priv-app/MiuiExtraPhoto
+rm -v -r /system/priv-app/MiuiFreeformService
+rm -v -r /system/priv-app/MiuiMusicGlobal
+rm -v -r /system/priv-app/MiuiScanner
+rm -v -r /system/priv-app/Weather
+rm -v -r /system/priv-app/YellowPage
 
 
-rm -r /vendor/data-app/GlobalFashiongallery
-rm -r /vendor/data-app/wps_lite
-rm -r /vendor/data-app/XMRemoteController
+rm -v -r /vendor/data-app/GlobalFashiongallery
+rm -v -r /vendor/data-app/wps_lite
+rm -v -r /vendor/data-app/XMRemoteController
 
