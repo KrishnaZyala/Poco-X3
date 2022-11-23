a5fb9a4e127e7fb82ad7c56768b4a110a4135f0a
Krishna Singh <KrishnaZyala@gmail.com>
2022-11-23 22:14:42 +0530

    dblot added

---
+++ dblot.sh
@@ -0,0 +1,66 @@
+mkdir -v -p ./cust/{app,cust,etc,lost+found,prebuilts}
+mv -v -f /cust/app ./cust/
+mv -v -f /cust/cust ./cust/
+mv -v -f /cust/etc ./cust/
+mv -v -f /cust/lost+found ./cust/
+mv -v -f /cust/prebuilts ./cust/
+
+mkdir -v -p ./product/{app,priv-app}
+mv -v -f /product/app/Chrome ./product/app/
+mv -v -f /product/app/Gmail2 ./product/app/
+mv -v -f /product/app/GoogleContacts ./product/app/
+mv -v -f /product/app/GoogleOne ./product/app/
+mv -v -f /product/app/GPayIndia ./product/app/
+mv -v -f /product/app/Maps ./product/app/
+mv -v -f /product/app/Messages ./product/app/
+mv -v -f /product/app/PhotoTable ./product/app/
+mv -v -f /product/app/YouTube ./product/app/
+
+mv -v -f /product/priv-app/AndroidAutoStub ./product/priv-app/
+mv -v -f /product/priv-app/GoogleAssistant ./product/priv-app/
+mv -v -f /product/priv-app/Wellbeing ./product/priv-app/
+
+mkdir -v -p ./system/{app,data-app,priv-app}
+mv -v -f /system/app/AnalyticsCore ./system/app/
+mv -v -f /system/app/Calculator ./system/app/
+mv -v -f /system/app/CloudService ./system/app/
+mv -v -f /system/app/facebook-appmanager ./system/app/
+mv -v -f /system/app/FileExplorerGlobal ./system/app/
+mv -v -f /system/app/InMipay ./system/app/
+mv -v -f /system/app/Lens ./system/app/
+mv -v -f /system/app/mi_connect_service ./system/app/
+mv -v -f /system/app/MiCloudSync ./system/app/
+mv -v -f /system/app/MiLinkService2 ./system/app/
+mv -v -f /system/app/MiPicks ./system/app/
+mv -v -f /system/app/MiPlayClient ./system/app/
+mv -v -f /system/app/MiuiBugReport ./system/app/
+mv -v -f /system/app/MiuiCompass ./system/app/
+mv -v -f /system/app/MiuiFrequentPhrase ./system/app/
+mv -v -f /system/app/Notes ./system/app/
+mv -v -f /system/app/PaymentService ./system/app/
+mv -v -f /system/app/PlayAutoInstallStubApp ./system/app/
+mv -v -f /system/app/SystemAppUpdater ./system/app/
+mv -v -f /system/app/XiaomiServiceFramework ./system/app/
+
+mv -v -f /system/data-app/MiCreditInStub ./system/data-app
+mv -v -f /system/data-app/MiMediaEditor ./system/data-app
+
+mv -v -f /system/priv-app/Calendar ./system/priv-app
+mv -v -f /system/priv-app/CleanMaster ./system/priv-app
+mv -v -f /system/priv-app/facebook-installer ./system/priv-app
+mv -v -f /system/priv-app/facebook-services ./system/priv-app
+mv -v -f /system/priv-app/MiCalendarGlobal ./system/priv-app
+mv -v -f /system/priv-app/MiDrop ./system/priv-app
+mv -v -f /system/priv-app/MiService ./system/priv-app
+mv -v -f /system/priv-app/MiShare ./system/priv-app
+mv -v -f /system/priv-app/MiuiExtraPhoto ./system/priv-app
+mv -v -f /system/priv-app/MiuiFreef ./omvService ./system/priv-app
+mv -v -f /system/priv-app/MiuiMusicGlobal ./system/priv-app
+mv -v -f /system/priv-app/MiuiScanner ./system/priv-app
+mv -v -f /system/priv-app/Weather ./system/priv-app
+mv -v -f /system/priv-app/YellowPage ./system/priv-app
+
+mkdir -v -p ./vendor/data-app
+mv -v -f /vendor/data-app/GlobalFashiongallery ./vendor/data-app
+mv -v -f /vendor/data-app/wps_lite ./vendor/data-app
+mv -v -f /vendor/data-app/XMRemoteController ./vendor/data-app
